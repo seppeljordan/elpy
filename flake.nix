ba23bb875a03c2daba5665d4a0a83ae0b04f22fe
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-05-14 13:42:46 +0200

    Sirst successful setup

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "elpy emacs package";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: let
+    systemDependent = flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShell = pkgs.mkShell {
+        buildInputs = (with pkgs.python3.pkgs; [ virtualenv ]) ++ (with pkgs; [ cask ]);
+      };
+    });
+  in systemDependent;
+}
