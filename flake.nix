62c6b0cfc23b7dca9a1e5bebd5584d75b1ee57d5
Sebastian Jordan <sebastian.jordan.mail@googlemail.com>
2021-05-15 22:36:04 +0200

    Properly format flake.nix

--- flake.nix
+++ flake.nix
@@ -7,13 +7,15 @@
     nixpkgs.url = "github:seppeljordan/nixpkgs/fix-cask";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: let
-    systemDependent = flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs { inherit system; };
-    in {
-      devShell = pkgs.mkShell {
-        buildInputs = (with pkgs.python3.pkgs; [ virtualenv ]) ++ (with pkgs; [ cask ]);
-      };
-    });
-  in systemDependent;
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+      systemDependent = flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = import nixpkgs { inherit system; };
+        in {
+          devShell = pkgs.mkShell {
+            buildInputs = (with pkgs.python3.pkgs; [ virtualenv ])
+              ++ (with pkgs; [ cask nixfmt ]);
+          };
+        });
+    in systemDependent;
 }
